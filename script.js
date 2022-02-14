2751f542ff27ba00bf9dc3bfb1672cf3a846d559
Maksim <nemaxa@mail.ru>
2022-02-14 15:45:53 +0300

    second

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
 let personalMovieDb = {
-  count: numberOfFilms,
+  count: [numberOfFilms],
   movies: {},
   actors: {},
   genres: [],
   privat: false
 };
-let lastFilm = prompt("Последний просмотренный фильм?", ""),
-  lastFilm2 = prompt("Последний просмотренный фильм?", ""),
-  evaluate = prompt("Как вы его оцените", ""),
-  evaluate2 = prompt("Как вы его оцените", "");
+    if (personalMovieDb.count==undefined || 
+     personalMovieDb.count=="") { 
+    alert ("Произошла ошибка", "");
+}
+    else if (personalMovieDb.count < 10) {
+    alert ("Просмотрено довольно мало фильмов","");
+}
+    else  if  (personalMovieDb.count >= 10 &&
+     personalMovieDb.count < 30 ) {
+     alert ("Вы классический зритель!", "");
+}
+     else if (personalMovieDb.count >= 30) {
+     alert (" Вы киноман!", "");
+}   
+   
+/*for (let i=0; i<2; i++ ) {
+       let lastFilm =prompt("Последний просмотренный фильм?", ""), 
+           evaluate = prompt("Как вы его оцените", ""); 
+          if (((lastFilm && evaluate) !== "" || (lastFilm && evaluate) !== null) && String.length <=50) {
+            personalMovieDb.movies[lastFilm] = evaluate;
+          }
+          else {i--;}
+}*/
 
-personalMovieDb.movies[lastFilm] = evaluate;
-personalMovieDb.movies[lastFilm2] = evaluate2;
+
+for (let i=0; i<2;) {
+  for(let lastFilm =prompt("Последний просмотренный фильм?", ""), 
+       evaluate = prompt("Как вы его оцените", ""); (lastFilm && evaluate) !== null &&
+       lastFilm.length < 50 && evaluate.length < 50 &&       
+       (lastFilm && evaluate) !== "";) {
+        personalMovieDb.movies[lastFilm] = evaluate;
+        i++;
+        break;
+       }
+ }
+
 console.log(personalMovieDb);
+
